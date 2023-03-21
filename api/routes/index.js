356cfc658dd093a14b104129ad147fbd004c9a48
ashrithyvs <ashrithy20@gmail.com>
2023-03-21 17:20:30 +0530

    reorganized dir

--- api/routes/index.js
+++
@@ -1,8 +0,0 @@
-const projectController = require("../controllers").project;
-
-module.exports = (app) => {
-  app.get("/health", (req, res) => {
-    res.status(200).send({ message: "API is working!" });
-  });
-  app.get("/", projectController.getAllProjects);
-};
